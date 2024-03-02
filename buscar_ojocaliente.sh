607e5558a40e387ad34fd8d00cb67d5c3aab9eda
EmmanuelHdez <84370797+EmmanuelHdez@users.noreply.github.com>
2024-03-02 10:46:59 -0600

    Update buscar_ojocaliente.sh

--- buscar_ojocaliente.sh
+++ buscar_ojocaliente.sh
@@ -5,4 +5,4 @@ archivo="Accidentes_ags_2021.csv"
 
 # Buscar y mostrar datos
 echo "ID,MOTOCICLETA"
-grep -i "ojocaliente" "$archivo" | awk -F ',' '{print $1 "," $7}' | column -s', ' -t
+grep -i "ojocaliente" "$archivo" | awk -F ',' '{print $1 "," $23}' | column -s', ' -t
