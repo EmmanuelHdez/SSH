7e256fd3b15875d458630d1d7210f9e1c2d863ec
EmmanuelHdez <84370797+EmmanuelHdez@users.noreply.github.com>
2024-03-02 10:45:53 -0600

    Update buscar_adolfo.sh

--- buscar_adolfo.sh
+++ buscar_adolfo.sh
@@ -5,5 +5,5 @@ archivo="Accidentes_ags_2021.csv"
 
 # Buscar y mostrar datos
 echo "ID,CAMIONETA"
-grep -i "adolfo" "$archivo" | awk -F ',' '{print $1 "," $7}' | column -s', ' -t
+grep -i "adolfo" "$archivo" | awk -F ',' '{print $1 "," $19}' | column -s', ' -t
 
